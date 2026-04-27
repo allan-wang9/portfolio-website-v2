a2be9e8ff3f6414fb3acff137b79527df9a4917b
Allan Wang <allan8898@gmail.com>
2026-04-26 21:31:24 -0400

    update copyright year

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="c-space pt-5 pb-5 border-t border-black-300 gap-5">
-            <p className="text-white-500">© 2025 Allan Wang | All rights reserved.</p>
+            <p className="text-white-500">© 2026 Allan Wang | All rights reserved.</p>
         </footer>
     );
 };
